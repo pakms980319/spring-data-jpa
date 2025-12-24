737538fb8637415e19a141c9c330da555e027261
pakms980319 <pakms980319@gmail.com>
2025-12-24 15:58:49 +0900

    섹션 3. 예제 도메인 모델 7. 예제 도메인 모델과 동작확인

--- src/main/generated/study/data_jpa/entity/QMember.java
+++ src/main/generated/study/data_jpa/entity/QMember.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,22 +18,37 @@ public class QMember extends EntityPathBase<Member> {
 
     private static final long serialVersionUID = 1469328066L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QMember member = new QMember("member1");
 
+    public final NumberPath<Integer> age = createNumber("age", Integer.class);
+
     public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final QTeam team;
 
     public final StringPath username = createString("username");
 
     public QMember(String variable) {
-        super(Member.class, forVariable(variable));
+        this(Member.class, forVariable(variable), INITS);
     }
 
     public QMember(Path<? extends Member> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QMember(PathMetadata metadata) {
-        super(Member.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMember(PathMetadata metadata, PathInits inits) {
+        this(Member.class, metadata, inits);
+    }
+
+    public QMember(Class<? extends Member> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.team = inits.isInitialized("team") ? new QTeam(forProperty("team")) : null;
     }
 
 }
