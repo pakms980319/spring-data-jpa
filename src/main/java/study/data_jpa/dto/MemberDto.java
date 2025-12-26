2d179c7455c18dd6a0e5696c76a470433264dd31
pakms980319 <pakms980319@gmail.com>
2025-12-26 16:05:12 +0900

    섹션 6. 확장 기능 26. Web 확장 - 페이징과 정렬

--- src/main/java/study/data_jpa/dto/MemberDto.java
+++ src/main/java/study/data_jpa/dto/MemberDto.java
@@ -2,6 +2,7 @@ package study.data_jpa.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import study.data_jpa.entity.Member;
 
 @Data
 @AllArgsConstructor
@@ -9,4 +10,12 @@ public class MemberDto {
 	private Long id;
 	private String username;
 	private String teamName;
+
+	public MemberDto(Member member) {
+		this.id = member.getId();
+		this.username = member.getUsername();
+		if (member.getTeam() != null) {
+			this.teamName = member.getTeam().getName();
+		}
+	}
 }
